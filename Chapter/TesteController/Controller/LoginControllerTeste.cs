c5a9720deab692cac35905e4e5dc8e5b737cbf16
Marlon HenriqueSF <marlonhenriquesilvafelix@gmail.com>
2023-02-13 20:20:31 -0300

    Aula de recuperação 13/02

---
+++ Chapter/TesteController/Controller/LoginControllerTeste.cs
@@ -0,0 +1,75 @@
+﻿using Chapter.Controllers;
+using Chapter.Interfaces;
+using Chapter.Models;
+using Chapter.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteController.Controller
+{
+    public class LoginControllerTeste
+    {
+        [Fact]
+        public void LoginController_Retornar_Usuario_Invalido()
+        {
+            var repositoryEspelhado = new Mock<IUsuarioRepository>();
+            
+            repositoryEspelhado.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns((Usuario)null);
+
+            var controller = new LoginController(repositoryEspelhado.Object);
+
+            LoginViewModel dadosUsuario = new LoginViewModel();
+
+            dadosUsuario.email = "batata@email.com";
+            dadosUsuario.senha = "Batata";
+
+            var resultado = controller.Login(dadosUsuario);
+
+            Assert.IsType<UnauthorizedObjectResult>(resultado);
+        }
+
+        [Fact]
+
+        public void LoginController_Retorna_Token()
+        {
+
+            Usuario usuarioRetornado = new Usuario();
+            usuarioRetornado.Email = "email@email.com";
+            usuarioRetornado.Senha = "1234";
+            usuarioRetornado.Tipo = "0";
+            usuarioRetornado.Id = 1;
+
+            var repositoryEspelhado = new Mock<IUsuarioRepository>();
+
+            repositoryEspelhado.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns(usuarioRetornado);
+
+            LoginViewModel dadosUsuario = new LoginViewModel();
+
+            dadosUsuario.email = "batata@email.com";
+            dadosUsuario.senha = "Batata";
+
+            var controller = new LoginController(repositoryEspelhado.Object);
+
+            string issueValido = "chapter.webapi";
+
+            OkObjectResult resultado = (OkObjectResult)controller.Login(dadosUsuario);
+
+            string tokenString = resultado.Value.ToString().Split(' ')[3];
+
+            var jwtHandler = new JwtSecurityTokenHandler();
+
+            var tokenJwt = jwtHandler.ReadJwtToken(tokenString);
+
+            Assert.Equal(IssuerValido, tokenJwt.Issuer);
+
+        }
+
+    }
+}
